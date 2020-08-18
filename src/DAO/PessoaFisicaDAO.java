9f23d4c8ac0ef11f9fef237fede41acccf9f0c69
Junior Degasperi <celso.gasperi@universo.univates.br>
2020-08-17 21:45:41 -0300

    Commit do cadastro de Cidades. Implementado classes cidade, estado, cidadeDAO, estadoDAO, pessoaFisica, entidade, pessoaFisicaDAO, entidadeDAO, tela de cadastro de cidades

---
+++ src/DAO/PessoaFisicaDAO.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Utils.HibernateUtil;
+import Entidade.PessoaFisica;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Degasperi
+ */
+public class PessoaFisicaDAO extends Dao<PessoaFisica> {
+    
+    public PessoaFisica findById(int id) {
+        Session sessao = HibernateUtil.getSessionFactory().openSession();
+        try {
+            Transaction transacao = sessao.beginTransaction();
+            PessoaFisica retorno = (PessoaFisica) sessao.get(PessoaFisica.class, id);
+            return retorno;
+        } catch (HibernateException hibEx) {
+            hibEx.printStackTrace();
+        } finally {
+            sessao.close();
+        }
+        return null;
+    }
+
+    public List<PessoaFisica> findAllByDescription(String criteria) {
+        Session sessao = HibernateUtil.getSessionFactory().openSession();
+        List<PessoaFisica> pessoaFisica = null;
+        try {
+            org.hibernate.Query q = sessao.createQuery("from PessoaFisica p WHERE nm_pessoa_fisica LIKE '%" + criteria + " ORDER BY id_pessoa_fisica");
+            pessoaFisica = q.list();
+            return pessoaFisica;
+        } catch (HibernateException hibEx) {
+            hibEx.printStackTrace();
+        } finally {
+            sessao.close();
+        }
+        return null;
+    }
+
+    public List<PessoaFisica> findAll() {
+        Session sessao = HibernateUtil.getSessionFactory().openSession();
+        List<PessoaFisica> pessoaFisica = null;
+        try {
+            org.hibernate.Query q = sessao.createQuery("from PessoaFisica ORDER BY id_pessoa_fisica");
+            pessoaFisica = q.list();
+            return pessoaFisica;
+        } catch (HibernateException hibEx) {
+            hibEx.printStackTrace();
+        } finally {
+            sessao.close();
+        }
+        return null;
+    }        
+   
+}
